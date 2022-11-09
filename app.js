e1765687deba49becc4b3bb47f432d1b65bd5273
Deondre Mayers <deondre-mayers@hotmail.com>
2022-11-09 09:44:27 -0400

    Update files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-    document.addEventListener("DOMContentLoaded", function() 
+document.addEventListener("DOMContentLoaded", function() 
     {
         let btn = document.getElementById("btn");
 
@@ -43,23 +43,3 @@
             
     }
 }*/
-/*$(document).ready(function() {
-    var loadBtn = $('#theBtn');
-
-    loadBtn.on('click', function() {
-
-        $.ajax('superheroes.php', {
-            method: "GET",
-            dataType: 'json'
-        }).done(function(response) {
-            var superheroes = response.superheroes;
-
-            $('#results').append("<ul></ul>");
-            $(superheroes).each(function() {
-                $('#results ul').append('<li><h3>' + this.name + ' (a.k.a ' + this.alias + ')</h3><p>' + this.email + '</p></li>');
-            });
-        }).fail(function() {
-            alert('There was a problem with the request.');
-        });
-    });
-});*/
