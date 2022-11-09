0d7194e71398613a810c9ec40fe3908a29de421e
Deondre Mayers <deondre-mayers@hotmail.com>
2022-11-09 12:48:10 -0400

    Updated files

--- app.js
+++ app.js
@@ -1,45 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() 
-    {
-        let btn = document.getElementById("btn");
+    window.onload = () => {
 
-        btn.addEventListener("click", function(event)
-        {
-            event.preventDefault();
-
-            fetch('http://localhost/comp2245-assignment4/superheroes.php')
-            .then(response => response.text())
-            .then(data => {
-
-                alert(data);
-            })
-            .catch(error =>
-                {
-                    alert(error);
-                })
-        })
-    })
-
-/*window.onload= function(){
-
-    let button= document.getElementById("btn");
-
-    button.addEventListener("click", function(e){
+        const btn = document.getElementById("search");
+        const textfield = document.getElementById("search");
+        const result = document.getElementById("result");
+        let httpRequest = new XMLHttpRequest();
+        let url = 'superheroes.php';
+      
+        btn.addEventListener('click', search);
+      
+      
+        function search(){
+          let data = textfield.value;
+          let ext = '?q='+data;
+          httpRequest.onreadystatechange = requestaction;
+          httpRequest.open('GET', url+ext, true);
+          httpRequest.send();
+        }
         
-        httpReq= new XMLHttpRequest();
-        let url ="http://localhost/comp2245-assignment4/superheroes.php?query="+searchAct.value+"";
-        httpReq.open('GET',url);
-        httpReq.send();
-        httpReq.onreadystatechange= reply;
-    });
-
-    function reply(){
-        if(httpReq.readyState=== XMLHttpRequest.DONE){
-            if(httpReq.status===200){
-                result.innerHTML=httpReq.responseText;
-            }else{
-                alert('There was an Error')
-                }
-            }
-            
-    }
-}*/
+        function requestaction(){
+          if(httpRequest.readyState === XMLHttpRequest.DONE){
+            if(httpRequest.status === 200){
+      
+                let response = httpRequest.responseText;
+                result.innerHTML = response;
+      
+              }else{
+      
+                alert('There is a problem with the request');
+      
+              }
+          }
+        }
+      }
+      
